2e21b4a74759114011ef71a42c817a9724cc0b2c
Aleksander Baska <baskaaleksander03@gmail.com>
2025-11-11 22:38:34 +0100

    Add global exception handlers for user not found, unauthorized, and invalid credentials

--- services/auth/src/main/java/com/baskaaleksander/nuvine/application/exception/GlobalExceptionHandler.java
+++ services/auth/src/main/java/com/baskaaleksander/nuvine/application/exception/GlobalExceptionHandler.java
@@ -1,7 +1,6 @@
 package com.baskaaleksander.nuvine.application.exception;
 
-import com.baskaaleksander.nuvine.domain.exception.EmailExistsException;
-import com.baskaaleksander.nuvine.domain.exception.ErrorResponse;
+import com.baskaaleksander.nuvine.domain.exception.*;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +11,45 @@ import java.time.Instant;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleUserNotFoundException(UserNotFoundException ex, HttpServletRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                404,
+                HttpStatus.NOT_FOUND.getReasonPhrase(),
+                ex.getMessage(),
+                request.getRequestURI(),
+                Instant.now()
+        );
+
+        return ResponseEntity.status(404).body(errorResponse);
+    }
+
+    @ExceptionHandler(UnauthorizedException.class)
+    public ResponseEntity<ErrorResponse> handleUnauthorizedException(UnauthorizedException ex, HttpServletRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                401,
+                HttpStatus.UNAUTHORIZED.getReasonPhrase(),
+                ex.getMessage(),
+                request.getRequestURI(),
+                Instant.now()
+        );
+
+        return ResponseEntity.status(401).body(errorResponse);
+    }
+
+    @ExceptionHandler(InvalidCredentialsException.class)
+    public ResponseEntity<ErrorResponse> handleInvalidCredentialsException(InvalidCredentialsException ex, HttpServletRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                401,
+                HttpStatus.UNAUTHORIZED.getReasonPhrase(),
+                ex.getMessage(),
+                request.getRequestURI(),
+                Instant.now()
+        );
+
+        return ResponseEntity.status(401).body(errorResponse);
+    }
 
     @ExceptionHandler(EmailExistsException.class)
     public ResponseEntity<ErrorResponse> handleEmailExistsException(EmailExistsException ex, HttpServletRequest request) {
