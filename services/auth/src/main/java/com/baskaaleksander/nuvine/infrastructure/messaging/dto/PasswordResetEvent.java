c0a90c276a9c1a9ca1bd692bf75634045eb81c55
Aleksander Baska <baskaaleksander03@gmail.com>
2025-11-15 20:59:03 +0100

    feat(auth): added unit tests for password reset event producer

--- services/auth/src/main/java/com/baskaaleksander/nuvine/infrastructure/messaging/dto/PasswordResetEvent.java
+++ services/auth/src/main/java/com/baskaaleksander/nuvine/infrastructure/messaging/dto/PasswordResetEvent.java
@@ -1,10 +1,9 @@
 package com.baskaaleksander.nuvine.infrastructure.messaging.dto;
 
-import java.util.UUID;
-
 public record PasswordResetEvent(
         String email,
         String token,
         String userId
 ) {
 }
+
