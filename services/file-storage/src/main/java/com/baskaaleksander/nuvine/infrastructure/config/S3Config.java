bab2da956df188dd9f896c57174a009795a63f9d
Aleksander Baska <baskaaleksander03@gmail.com>
2025-11-19 15:50:24 +0100

    feat(fs): added s3 config

---
+++ services/file-storage/src/main/java/com/baskaaleksander/nuvine/infrastructure/config/S3Config.java
@@ -0,0 +1,61 @@
+package com.baskaaleksander.nuvine.infrastructure.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.S3Configuration;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+
+import java.net.URI;
+
+@Configuration
+public class S3Config {
+
+    @Value("${s3.access.secret}")
+    private String secret;
+    @Value("${s3.access.key}")
+    private String accessKey;
+    @Value("${s3.external-url}")
+    private String externalUrl;
+    @Value("${s3.internal-url}")
+    private String internalUrl;
+    @Value("${s3.bucket-name}")
+    private String bucket;
+
+
+    @Bean
+    public S3Presigner s3Presigner() {
+        AwsBasicCredentials creds = AwsBasicCredentials.create(secret, accessKey);
+
+        return S3Presigner.builder()
+                .endpointOverride(URI.create(externalUrl))
+                .region(Region.US_EAST_1)
+                .credentialsProvider(StaticCredentialsProvider.create(creds))
+                .serviceConfiguration(
+                        S3Configuration.builder()
+                                .pathStyleAccessEnabled(true)
+                                .build()
+                )
+                .build();
+    }
+
+    @Bean
+    public S3Client s3Client() {
+        AwsBasicCredentials awsBasicCredentials = AwsBasicCredentials.create(secret, accessKey);
+
+        return S3Client.builder()
+                .endpointOverride(URI.create(internalUrl))
+                .credentialsProvider(StaticCredentialsProvider.create(awsBasicCredentials))
+                .region(Region.US_EAST_1)
+                .serviceConfiguration(
+                        S3Configuration.builder()
+                                .pathStyleAccessEnabled(true)
+                                .build()
+                )
+                .build();
+    }
+}
