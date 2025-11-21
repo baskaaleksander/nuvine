fc7534533c658f0747efa0113528f8bf72f4bd85
Aleksander Baska <baskaaleksander03@gmail.com>
2025-11-21 22:50:47 +0100

    feat: created document uploaded event and it's consumer

--- services/ingestion/src/main/java/com/baskaaleksander/nuvine/infrastructure/messaging/DocumentUploadedEventConsumer.java
+++ services/ingestion/src/main/java/com/baskaaleksander/nuvine/infrastructure/messaging/DocumentUploadedEventConsumer.java
@@ -1,5 +1,19 @@
 package com.baskaaleksander.nuvine.infrastructure.messaging;
 
+import com.baskaaleksander.nuvine.infrastructure.messaging.dto.DocumentUploadedEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
 
+@Service
+@Slf4j
+@RequiredArgsConstructor
 public class DocumentUploadedEventConsumer {
+f
+    @KafkaListener(topics = "${topics.document-uploaded-topic}", groupId = "${spring.kafka.consumer.group-id:ingestion-service}")
+    public void consumeDocumentUploadedEvent(DocumentUploadedEvent event) {
+        log.info("Consumed document uploaded event: {}", event);
+    }
+
 }
