9de39272bd19fa403fb210e1844e46763d245c00
Aleksander Baska <baskaaleksander03@gmail.com>
2025-11-21 22:35:59 +0100

    feat: stage and status enums

--- services/ingestion/src/main/java/com/baskaaleksander/nuvine/domain/model/IngestionStage.java
+++ services/ingestion/src/main/java/com/baskaaleksander/nuvine/domain/model/IngestionStage.java
@@ -1,4 +1,5 @@
 package com.baskaaleksander.nuvine.domain.model;
 
 public enum IngestionStage {
+    QUEUED, FETCH, OCR, PARSE, CHUNK, EMBED, GRAPH
 }
--- services/ingestion/src/main/java/com/baskaaleksander/nuvine/domain/model/IngestionStatus.java
+++ services/ingestion/src/main/java/com/baskaaleksander/nuvine/domain/model/IngestionStatus.java
@@ -1,4 +1,5 @@
 package com.baskaaleksander.nuvine.domain.model;
 
 public enum IngestionStatus {
+    QUEUED, PROCESSING, COMPLETED, FAILED
 }
