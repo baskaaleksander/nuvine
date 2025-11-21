4c313bc7e075d8e217ce469c497fa5d626e03621
Aleksander Baska <baskaaleksander03@gmail.com>
2025-11-21 22:56:11 +0100

    feat: added basic processing of event by saving it in db

---
+++ services/ingestion/src/main/java/com/baskaaleksander/nuvine/domain/service/IngestionService.java
@@ -0,0 +1,34 @@
+package com.baskaaleksander.nuvine.domain.service;
+
+import com.baskaaleksander.nuvine.domain.model.IngestionJob;
+import com.baskaaleksander.nuvine.domain.model.IngestionStage;
+import com.baskaaleksander.nuvine.domain.model.IngestionStatus;
+import com.baskaaleksander.nuvine.infrastructure.messaging.dto.DocumentUploadedEvent;
+import com.baskaaleksander.nuvine.infrastructure.repository.IngestionJobRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import static java.util.UUID.fromString;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class IngestionService {
+
+    private final IngestionJobRepository ingestionJobRepository;
+
+    public void process(DocumentUploadedEvent event) {
+        IngestionJob job = IngestionJob.builder()
+                .documentId(fromString(event.documentId()))
+                .workspaceId(fromString(event.workspaceId()))
+                .projectId(fromString(event.projectId()))
+                .storageKey(event.storageKey())
+                .mimeType(event.mimeType())
+                .status(IngestionStatus.COMPLETED)
+                .stage(IngestionStage.QUEUED)
+                .build();
+
+        ingestionJobRepository.save(job);
+    }
+}
--- services/ingestion/src/main/java/com/baskaaleksander/nuvine/infrastructure/messaging/DocumentUploadedEventConsumer.java
+++ services/ingestion/src/main/java/com/baskaaleksander/nuvine/infrastructure/messaging/DocumentUploadedEventConsumer.java
@@ -1,5 +1,6 @@
 package com.baskaaleksander.nuvine.infrastructure.messaging;
 
+import com.baskaaleksander.nuvine.domain.service.IngestionService;
 import com.baskaaleksander.nuvine.infrastructure.messaging.dto.DocumentUploadedEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -10,10 +11,14 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @RequiredArgsConstructor
 public class DocumentUploadedEventConsumer {
-f
+
+    private final IngestionService ingestionService;
+
     @KafkaListener(topics = "${topics.document-uploaded-topic}", groupId = "${spring.kafka.consumer.group-id:ingestion-service}")
     public void consumeDocumentUploadedEvent(DocumentUploadedEvent event) {
-        log.info("Consumed document uploaded event: {}", event);
+        log.info("DOCUMENT_UPLOADED_EVENT RECEIVED documentId={}", event.documentId());
+        ingestionService.process(event);
+        log.info("DOCUMENT_UPLOAD_EVENT HANDLED documentId={}", event.documentId());
     }
 
 }
