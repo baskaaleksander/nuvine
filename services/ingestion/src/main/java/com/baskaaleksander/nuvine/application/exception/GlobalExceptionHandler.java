41543d5a906a46043f3ac4c592b5e36a2f39c037
Aleksander Baska <baskaaleksander03@gmail.com>
2025-11-23 20:50:03 +0100

    feat: added global exception handler

---
+++ services/ingestion/src/main/java/com/baskaaleksander/nuvine/application/exception/GlobalExceptionHandler.java
@@ -0,0 +1,41 @@
+package com.baskaaleksander.nuvine.application.exception;
+
+import com.baskaaleksander.nuvine.domain.exception.ErrorResponse;
+import com.baskaaleksander.nuvine.domain.exception.IngestionJobNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.Instant;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IngestionJobNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleIngestionJobNotFoundException(IngestionJobNotFoundException ex, HttpServletRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                404,
+                HttpStatus.NOT_FOUND.getReasonPhrase(),
+                ex.getMessage(),
+                request.getRequestURI(),
+                Instant.now()
+        );
+
+        return ResponseEntity.status(404).body(errorResponse);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleGenericException(Exception ex, HttpServletRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                500,
+                HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(),
+                ex.getMessage(),
+                request.getRequestURI(),
+                Instant.now()
+        );
+
+        return ResponseEntity.status(500).body(errorResponse);
+    }
+}
