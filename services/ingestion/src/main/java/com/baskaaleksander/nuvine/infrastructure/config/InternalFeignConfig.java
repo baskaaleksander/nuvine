e5f1b7f54f027287167350c29992746f7177a217
Aleksander Baska <baskaaleksander03@gmail.com>
2025-11-24 18:38:27 +0100

    feat: forwarding service account token instead of duping logging into service acc

--- services/ingestion/src/main/java/com/baskaaleksander/nuvine/infrastructure/config/InternalFeignConfig.java
+++ services/ingestion/src/main/java/com/baskaaleksander/nuvine/infrastructure/config/InternalFeignConfig.java
@@ -1,26 +1,28 @@
 package com.baskaaleksander.nuvine.infrastructure.config;
 
-import com.baskaaleksander.nuvine.infrastructure.auth.KeycloakClientCredentialsTokenProvider;
 import feign.RequestInterceptor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 
 @Configuration
 @RequiredArgsConstructor
 @Slf4j
 public class InternalFeignConfig {
 
-    private final KeycloakClientCredentialsTokenProvider tokenProvider;
-
+    // as the only allowed to use these routes is internal service, we can forward the token
     @Bean
-    public RequestInterceptor internalTokenForwardingInterceptor() {
-
+    public RequestInterceptor userTokenForwardingInterceptor() {
         return template -> {
-            var token = tokenProvider.getAccessToken();
-            template.header("Authorization", "Bearer " + token);
-        };
+            var auth = SecurityContextHolder.getContext().getAuthentication();
 
+            if (auth instanceof JwtAuthenticationToken jwtAuth) {
+                String tokenValue = jwtAuth.getToken().getTokenValue();
+                template.header("Authorization", "Bearer " + tokenValue);
+            }
+        };
     }
 }
