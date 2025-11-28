e519710f4fdd517649bdf638a33395e206d5bbee
Aleksander Baska <baskaaleksander03@gmail.com>
2025-11-28 10:57:12 +0100

    feat: created internal controller for vector search

---
+++ services/vector/src/main/java/com/baskaaleksander/nuvine/application/controller/VectorInternalController.java
@@ -0,0 +1,34 @@
+package com.baskaaleksander.nuvine.application.controller;
+
+import com.baskaaleksander.nuvine.application.dto.TextVectorSearchRequest;
+import com.baskaaleksander.nuvine.application.dto.VectorSearchRequest;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/internal/vector")
+@RequiredArgsConstructor
+@PreAuthorize("hasRole('INTERNAL_SERVICE')")
+public class VectorInternalController {
+
+
+    @PostMapping("/search-by-text")
+    public ResponseEntity<?> searchByText(
+            @RequestBody @Valid TextVectorSearchRequest request
+    ) {
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/search")
+    public ResponseEntity<?> search(
+            @RequestBody @Valid VectorSearchRequest request
+    ) {
+        return ResponseEntity.ok().build();
+    }
+}
---
+++ services/vector/src/main/java/com/baskaaleksander/nuvine/application/dto/TextVectorSearchRequest.java
@@ -0,0 +1,25 @@
+package com.baskaaleksander.nuvine.application.dto;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.util.List;
+import java.util.UUID;
+
+public record TextVectorSearchRequest(
+        @NotNull(message = "Workspace Id cannot be null")
+        UUID workspaceId,
+        @NotNull(message = "Project Id cannot be null")
+        UUID projectId,
+        @NotEmpty(message = "Document Ids cannot be empty")
+        List<UUID> documentIds,
+        @NotNull(message = "Query cannot be null")
+        String query,
+        @NotNull(message = "Top K cannot be null")
+        @Size(min = 8, max = 14, message = "Top K must be between 8 and 14")
+        int topK,
+        @Size(min = 0, max = 1, message = "Threshold must be between 0 and 1")
+        long threshold
+) {
+}
