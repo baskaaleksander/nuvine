4df9194097d8398b42c219a1e1422bdb092a5c00
Aleksander Baska <baskaaleksander03@gmail.com>
2025-11-30 18:52:25 +0100

    feat: added status orchestrator for ingestion job

--- services/ingestion/src/main/java/com/baskaaleksander/nuvine/infrastructure/messaging/in/VectorProcessingCompletedEventConsumer.java
+++ services/ingestion/src/main/java/com/baskaaleksander/nuvine/infrastructure/messaging/in/VectorProcessingCompletedEventConsumer.java
@@ -1,5 +1,6 @@
 package com.baskaaleksander.nuvine.infrastructure.messaging.in;
 
+import com.baskaaleksander.nuvine.domain.service.IngestionStatusOrchestrator;
 import com.baskaaleksander.nuvine.infrastructure.messaging.dto.VectorProcessingCompletedEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,8 +12,12 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class VectorProcessingCompletedEventConsumer {
 
+    private IngestionStatusOrchestrator ingestionStatusOrchestrator;
+
     @KafkaListener(topics = "${topics.vector-processing-completed-topic}")
     public void consumeVectorProcessingCompletedEvent(VectorProcessingCompletedEvent event) {
         log.info("VECTOR_PROCESSING_COMPLETED_EVENT received embeddingJobId={} documentId={} projectId={} workspaceId={}", event.ingestionJobId(), event.documentId(), event.projectId(), event.workspaceId());
+        ingestionStatusOrchestrator.handleVectorProcessingCompleted(event.ingestionJobId());
+        log.info("VECTOR_PROCESSING_COMPLETED_EVENT processed embeddingJobId={} documentId={} projectId={} workspaceId={}", event.ingestionJobId(), event.documentId(), event.projectId(), event.workspaceId());
     }
 }
