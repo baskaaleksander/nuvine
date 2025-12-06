87017f3016f0734b0aff76c683b52114d4db44d4
Aleksander Baska <baskaaleksander03@gmail.com>
2025-12-06 12:29:01 +0100

    feat: added handling of subscription created

--- services/subscription/src/main/java/com/baskaaleksander/nuvine/domain/service/WebhookService.java
+++ services/subscription/src/main/java/com/baskaaleksander/nuvine/domain/service/WebhookService.java
@@ -1,17 +1,29 @@
 package com.baskaaleksander.nuvine.domain.service;
 
+import com.baskaaleksander.nuvine.domain.model.Subscription;
+import com.baskaaleksander.nuvine.infrastructure.persistence.SubscriptionRepository;
 import com.stripe.model.Event;
+import com.stripe.model.EventDataObjectDeserializer;
+import com.stripe.model.SubscriptionItem;
+import com.stripe.model.checkout.Session;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.Map;
+import java.util.UUID;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class WebhookService {
 
+    private final SubscriptionRepository subscriptionRepository;
+
+    private final
+
     public void handleEvent(Event event) {
-        log.info("Received event: {}", event);
         switch (event.getType()) {
             case "customer.subscription.created" -> handleCustomerSubscriptionCreated(event);
             case "customer.subscription.updated" -> handleCustomerSubscriptionUpdated(event);
@@ -35,7 +47,17 @@ public class WebhookService {
     }
 
     private void handleCheckoutSessionCompleted(Event event) {
-        log.info("Checkout session completed: {}", event.toJson());
+        EventDataObjectDeserializer eventDataObjectDeserializer = event.getDataObjectDeserializer();
+        Session checkoutSession;
+
+        try {
+            checkoutSession = (com.stripe.model.checkout.Session) eventDataObjectDeserializer.getObject().get();
+        } catch (Exception e) {
+            log.error("Failed to deserialize event data", e);
+            return;
+        }
+
+        String id = checkoutSession.getId();
     }
 
     private void handleChargeRefunded(Event event) {
@@ -76,7 +98,42 @@ public class WebhookService {
     }
 
     private void handleCustomerSubscriptionCreated(Event event) {
-        log.info("Customer subscription created: {}", event.toJson());
+        EventDataObjectDeserializer eventDataObjectDeserializer = event.getDataObjectDeserializer();
+        com.stripe.model.Subscription stripeSubscription;
+
+        try {
+            stripeSubscription = (com.stripe.model.Subscription) eventDataObjectDeserializer.getObject().get();
+        } catch (Exception e) {
+            log.error("Failed to deserialize event data", e);
+            return;
+        }
+
+        Map<String, String> metadata = stripeSubscription.getMetadata();
+        String workspaceId = metadata.get("workspace_id");
+        String planId = metadata.get("plan_id");
+
+        String stripeCustomerId = stripeSubscription.getCustomer();
+        String stripeSubscriptionId = stripeSubscription.getId();
+        Boolean cancelAtPeriodEnd = stripeSubscription.getCancelAtPeriodEnd();
+
+        SubscriptionItem item = stripeSubscription.getItems().getData().get(0);
+        Long currentPeriodStart = item.getCurrentPeriodStart();
+        Long currentPeriodEnd = item.getCurrentPeriodEnd();
+
+
+        Subscription subscription = Subscription.builder()
+                .workspaceId(UUID.fromString(workspaceId))
+                .planId(UUID.fromString(planId))
+                .stripeCustomerId(stripeCustomerId)
+                .stripeSubscriptionId(stripeSubscriptionId)
+                .cancelAtPeriodEnd(cancelAtPeriodEnd)
+                .currentPeriodStart(Instant.ofEpochSecond(currentPeriodStart))
+                .currentPeriodEnd(Instant.ofEpochSecond(currentPeriodEnd))
+                .build();
+
+        //todo call to workspace service
+
+        subscriptionRepository.save(subscription);
     }
 
 }
