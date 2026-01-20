b833f56a89a53bdae651c9f4f42a375f88955fea
Aleksander Baska <baskaaleksander03@gmail.com>
2026-01-20 22:02:49 +0100

    feat: added 2 domain exceptions to geh

--- services/ingestion/src/main/java/com/baskaaleksander/nuvine/application/exception/GlobalExceptionHandler.java
+++ services/ingestion/src/main/java/com/baskaaleksander/nuvine/application/exception/GlobalExceptionHandler.java
@@ -1,8 +1,10 @@
 package com.baskaaleksander.nuvine.application.exception;
 
+import com.baskaaleksander.nuvine.domain.exception.DocumentNotFoundException;
 import com.baskaaleksander.nuvine.domain.exception.ErrorResponse;
 import com.baskaaleksander.nuvine.domain.exception.IngestionJobConflictException;
 import com.baskaaleksander.nuvine.domain.exception.IngestionJobNotFoundException;
+import com.baskaaleksander.nuvine.domain.exception.UnauthorizedDocumentAccessException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,7 +12,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.Instant;
-import java.util.Arrays;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -41,11 +42,34 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(404).body(errorResponse);
     }
 
+    @ExceptionHandler(DocumentNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleDocumentNotFoundException(DocumentNotFoundException ex, HttpServletRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                404,
+                HttpStatus.NOT_FOUND.getReasonPhrase(),
+                ex.getMessage(),
+                request.getRequestURI(),
+                Instant.now()
+        );
+
+        return ResponseEntity.status(404).body(errorResponse);
+    }
+
+    @ExceptionHandler(UnauthorizedDocumentAccessException.class)
+    public ResponseEntity<ErrorResponse> handleUnauthorizedDocumentAccessException(UnauthorizedDocumentAccessException ex, HttpServletRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                403,
+                HttpStatus.FORBIDDEN.getReasonPhrase(),
+                ex.getMessage(),
+                request.getRequestURI(),
+                Instant.now()
+        );
+
+        return ResponseEntity.status(403).body(errorResponse);
+    }
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleGenericException(Exception ex, HttpServletRequest request) {
-        System.out.println(ex.getMessage());
-        System.out.println(ex.getClass());
-        System.out.println(Arrays.toString(ex.getStackTrace()));
         ErrorResponse errorResponse = new ErrorResponse(
                 500,
                 HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(),
